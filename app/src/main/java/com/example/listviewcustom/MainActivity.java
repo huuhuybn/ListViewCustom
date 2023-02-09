d7bb6ec5dd6c1748bf2e10fa42dd67151083c1f5
HuyNguyen <huuhuybn@gmail.com>
2023-02-09 15:35:07 +0700

    Initial commit

---
+++ app/src/main/java/com/example/listviewcustom/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.listviewcustom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    // ListView : là 1 view group  hỗ trợ hiển thị 1 array dữ liệu
+    // array thì có thể là string array, int array hoặc object array
+    // String name[] = {"TUNG","KIEN","THANG","AN"};
+    // Student array , Car Array
+    // 1 DATA
+    // ListView trong xml
+    // Adapter
+    String name[] = {"TUNG","KIEN","THANG","AN","KIEN","THANG","AN","KIEN","THANG","AN","KIEN","THANG","AN","KIEN","THANG","AN","KIEN","THANG","AN","KIEN","THANG","AN","KIEN","THANG","AN","KIEN","THANG","AN"}; // data
+    ListView lvData; // khai bao 1 bien
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        lvData = findViewById(R.id.lvData);
+        ArrayList<Student> danhSach = new ArrayList<>();
+
+        for (int i = 0; i < 100; i++) {
+            Student student = new Student("HUY " + i,"09123456" + i);
+            danhSach.add(student);
+        }
+
+        StudentAdapter studentAdapter =
+                new StudentAdapter(danhSach);
+        lvData.setAdapter(studentAdapter);
+
+        /*ArrayAdapter<String> nameArray =
+                new ArrayAdapter(MainActivity.this,
+                        android.R.layout.simple_list_item_1,name);
+        lvData.setAdapter(nameArray);
+        lvData.setOnItemClickListener((adapterView, view, i, l) -> {
+
+        });
+*/
+
+    }
+}
---
+++ app/src/main/java/com/example/listviewcustom/StudentAdapter.java
@@ -0,0 +1,46 @@
+package com.example.listviewcustom;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class StudentAdapter extends BaseAdapter {
+
+    private ArrayList<Student> danhSach;
+    public StudentAdapter(ArrayList<Student> danhSach){
+        this.danhSach = danhSach;
+    }
+
+    @Override
+    public int getCount() {
+        return danhSach.size(); // định nghĩa số lượng item mà adapter sẽ xử lý
+    }
+
+    @Override
+    public Student getItem(int i) {
+        return danhSach.get(i); // cách lấy dữ liệu của 1 hàng
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) { // định nghĩa giao diện cho 1 hàng, và thiết lập dữ liệu hiển thị
+        view = LayoutInflater.
+                from(viewGroup.getContext()).inflate(R.layout.row_student,
+                        viewGroup,false);
+        TextView tvName = view.findViewById(R.id.tvName);
+        TextView tvPhone = view.findViewById(R.id.tvPhone);
+        Student student = getItem(i);
+        tvName.setText(student.getName());
+        tvPhone.setText(student.getPhone());
+        // cách gán gía trị đơn giản nhưng chưa tối ưu cho ListView
+        return view;
+    }
+}
